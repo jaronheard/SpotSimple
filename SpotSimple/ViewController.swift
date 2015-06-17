93473bef612068736a7710b815a4114356f73b3d
Jaron Heard <jaronheard@gmail.com>
2015-06-17 13:06:41 -0700

    Initial commit - login button redirecting to Spotify authentication but callback not yet implemented

--- SpotSimple/ViewController.swift
+++ SpotSimple/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let kClientID = "3232e7f472f34d1fa31e360e229f7df2"
+    let kCallbackURL = "Spotsimple://returnAfterLogin"
+    let kTokenSwapURL = "http://localhost:1234/swap"
+    let kTokenRefreshServiceURL = "http://localhost:1234/refresh"
+    
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func LoginWithSpotify(sender: AnyObject) {
+        let auth = SPTAuth.defaultInstance()
+        let loginURL = SPTAuth.loginURLForClientId(kClientID, withRedirectURL: NSURL(string: kCallbackURL), scopes: [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthUserLibraryReadScope], responseType: "code")
+        UIApplication.sharedApplication().openURL(loginURL)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
