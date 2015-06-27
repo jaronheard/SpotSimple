5af899e728090b66a1311f2aa9042319b30ffd76
Jaron Heard <jaronheard@gmail.com>
2015-06-26 18:03:42 -0700

    refactor session setup, player setup, initial playback

--- SpotSimple/ViewController.swift
+++ SpotSimple/ViewController.swift
@@ -27,25 +27,9 @@ class ViewController: UIViewController {
         
         if let sessionObject:AnyObject = userDefaults.objectForKey("spotifySession") {
             //session available
-            if let sessionDataObject = sessionObject as? NSData {
-                if let session = NSKeyedUnarchiver.unarchiveObjectWithData(sessionDataObject) as? SPTSession {
-                    if !session.isValid() {
-                        SPTAuth.defaultInstance().renewSession(self.session, callback: { (error: NSError!,renewedSession: SPTSession!) -> Void in
-                            if let errorCheck = error {
-                                println("Error refreshing session")
-                                return
-                            }
-                            if renewedSession != nil {
-                                self.session = renewedSession!
-                                syncSession(self.session)
-                            }
-                        })
-                    } else {
-                        SPTAuth.defaultInstance().session = session
-                    }
-                }
-            }
-            handleNewSession()
+            setupSession(sessionObject)
+            setupPlayer()
+            playPlaylistFirstPageFromURI("spotify:user:cariboutheband:playlist:4Dg0J0ICj9kKTGDyFu0Cv4")
         } else {
             LoginButton.hidden = false
         }
@@ -53,6 +37,8 @@ class ViewController: UIViewController {
     
     func updateAfterFirstLogin() {
         LoginButton.hidden = true
+        setupPlayer()
+        playPlaylistFirstPageFromURI("spotify:user:cariboutheband:playlist:4Dg0J0ICj9kKTGDyFu0Cv4")
     }
     
     func playUsingSession(session: SPTSession!) {
@@ -97,7 +83,7 @@ SPTYourMusic.savedTracksForUserWithAccessToken(session.accessToken, callback: { 
         return library
     }
     
-    func handleNewSession() {
+    func setupPlayer() {
         if self.player == nil {
             self.player = SPTAudioStreamingController(clientId: SPTAuth.defaultInstance().clientID!)
             self.player.playbackDelegate = self.player.playbackDelegate //not sure about this line
@@ -126,38 +112,45 @@ SPTYourMusic.savedTracksForUserWithAccessToken(session.accessToken, callback: { 
                 let followers = user.followerCount
                 let ds = 1
             })
-*/            
-            let playlistReq = SPTPlaylistSnapshot.createRequestForPlaylistWithURI(NSURL(string: "spotify:user:cariboutheband:playlist:4Dg0J0ICj9kKTGDyFu0Cv4"), accessToken: SPTAuth.defaultInstance().session.accessToken, error: nil)
-            SPTRequest.sharedHandler().performRequest(playlistReq, callback: { (error: NSError!, response: NSURLResponse!, data: NSData!) -> Void in
-                if error != nil {
-                    println("enabling playback got error \(error)")
-                    return
-                }
-                let playlistSnapshot = SPTPlaylistSnapshot(fromData: data, withResponse: response, error: nil)
-                self.player.playURIs(playlistSnapshot.firstTrackPage.items, fromIndex: 0, callback: nil)
-/* testcode
-                let seconds = 4.0
-                let delay = seconds * Double(NSEC_PER_SEC)  // nanoseconds per seconds
-                var dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-                
-                dispatch_after(dispatchTime, dispatch_get_main_queue(), {
-                    
-                    let trackURI = self.player.currentTrackURI
-                    let trackDur = self.player.currentTrackDuration
-                    let bd = 1
-                    
-                })
 */
-            })
         }
     }
     
-    func displayUserAlbums() {
-        //do stuff
+    func playPlaylistFirstPageFromURI(uri: String) {
+        let playlistReq = SPTPlaylistSnapshot.createRequestForPlaylistWithURI(NSURL(string:uri), accessToken: SPTAuth.defaultInstance().session.accessToken, error: nil)
+        SPTRequest.sharedHandler().performRequest(playlistReq, callback: { (error: NSError!, response: NSURLResponse!, data: NSData!) -> Void in
+            if error != nil {
+                println("enabling playback got error \(error)")
+                return
+            }
+            let playlistSnapshot = SPTPlaylistSnapshot(fromData: data, withResponse: response, error: nil)
+            self.player.playURIs(playlistSnapshot.firstTrackPage.items, fromIndex: 0, callback: nil)
+        })
     }
     
-    func loginWithSession() {
-        
+    func setupSession(sessionObject: AnyObject) {
+        if let sessionDataObject = sessionObject as? NSData {
+            if let session = NSKeyedUnarchiver.unarchiveObjectWithData(sessionDataObject) as? SPTSession {
+                if !session.isValid() {
+                    refreshSession()
+                } else {
+                    SPTAuth.defaultInstance().session = session
+                }
+            }
+        }
+    }
+    
+    func refreshSession() {
+        SPTAuth.defaultInstance().renewSession(SPTAuth.defaultInstance().session, callback: { (error: NSError!,renewedSession: SPTSession!) -> Void in
+            if let errorCheck = error {
+                println("Error refreshing session")
+                return
+            }
+            if renewedSession != nil {
+                SPTAuth.defaultInstance().session = renewedSession!
+                syncSession(SPTAuth.defaultInstance().session)
+            }
+        })
     }
     
     override func didReceiveMemoryWarning() {
